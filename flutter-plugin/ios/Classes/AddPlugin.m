32d7b9ac7af587dbfc3f8a6d5c931db28f3cd0fd
Aaron Boodman <aaron@aaronboodman.com>
2019-08-30 11:16:42 -1000

    The result of calling:
    flutter create -t plugin --org=dev.roci --project-name=add .

---
+++ flutter-plugin/ios/Classes/AddPlugin.m
@@ -0,0 +1,20 @@
+#import "AddPlugin.h"
+
+@implementation AddPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"add"
+            binaryMessenger:[registrar messenger]];
+  AddPlugin* instance = [[AddPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
